9fed92c6ef400be84a38fbb7bd6f31ba4ef12383
Collin Eggert <ahelper2@gmail.com>
2015-11-23 04:38:48 -0600

    Adding a bit of process handling code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var child = require('child_process').spawn,
-    args = require('yargs');
+    args = require('yargs'),
+    through = require('through');
 
 function usage() {
   console.log('usage: cliwrapper [--start startscript.js] [--signal #,signalscript.js] command arguments...');
@@ -10,4 +11,26 @@ if(args._.length == 0) {
   usage();
 }
 
-console.log('X');0
+console.log('X');
+
+var proc = spawn(args._.join(' '));
+
+proc.on('close', function(code, sig) {
+  console.log('process closed with code ' + code + ' and signal ' + sig);
+});
+
+proc.on('error', function(err) {
+  console.log(err);
+});
+
+proc.on('disconnect', function() {
+  console.log('disconnected');
+});
+
+proc.on('exit', function(code, sig) {
+  console.log('process exited with code ' + code + ' and signal ' + sig);
+});
+
+proc.stdout.on('data', function(chunk) {
+  console.log(chunk);
+});
